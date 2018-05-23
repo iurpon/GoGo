20d15dcd5329e9d1834f529c01ec94ee14c94763
Your Name <your.mail@host.com>
2018-05-23 09:48:58 +0300

    logging

--- src/main/java/ru/firstproject/web/LunchServlet.java
+++ src/main/java/ru/firstproject/web/LunchServlet.java
@@ -1,5 +1,8 @@
 package ru.firstproject.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,8 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LunchServlet extends HttpServlet {
+    private  static final Logger logger = LoggerFactory.getLogger(LunchServlet.class);
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.debug("forward lunch.jsp");
+        logger.info("ololollollllllllllllllllllllllllllllllll");
         request.getRequestDispatcher("/lunch.jsp").forward(request, response);
     }
 }
