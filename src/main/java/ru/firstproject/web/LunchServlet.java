420b47b58a7bffc4dbae86c64c3491ee3de34e44
Your Name <your.mail@host.com>
2018-05-23 05:53:32 +0300

    starting web(web.xml,servlet api)

---
+++ src/main/java/ru/firstproject/web/LunchServlet.java
@@ -0,0 +1,14 @@
+package ru.firstproject.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LunchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/lunch.jsp").forward(request, response);
+    }
+}
